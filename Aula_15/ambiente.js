9050ff8281878e55456d807194769fb3f8a8f7d1
DevMaygol <miguel,mvb@protonmail.com>
2022-01-21 11:16:41 -0300

    novas aulas

--- Aula_15/ambiente.js
+++ Aula_15/ambiente.js
@@ -8,8 +8,8 @@ console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
-let pos = num.indexOf(8)
-if(pos == -1){
+let pos = num.indexOf(11) //mostra o indice do elemento do vetor
+if(pos == -1){ //se o indice não for encontrado, função responde -1
     console.log('O valor não foi encontrado')
 } else{
     console.log(`O valor está na posição ${pos}`)
