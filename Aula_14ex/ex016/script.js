7ceb17403929654c753f36bdfc60b12b002e12a3
DevMaygol <miguel,mvb@protonmail.com>
2022-01-20 22:46:30 -0300

    resolvendo os exercícios

--- Aula_14ex/ex016/script.js
+++ Aula_14ex/ex016/script.js
@@ -1,33 +1,33 @@
 function contar(){
     var fpas = document.getElementById('txtpas')
     var fini = document.getElementById('txtini')
-    var ffim = document.querySelector('div#txtfim')
+    var ffim = document.getElementById('txtfim')
+    //var res = document.getElementById('res')
     
-    /*if (fini.value.length == 0){
+    var s = inicio + passo - passo
+    s = s + passo
+    
+    if (fini.value.length == 0 || ffim.value.length == 0 || fpas.value.length == 0){
         res.innerHTML = 'Impossível contar!'
-        window.alert('valor zerado')
-    } else if (fpas.value == 0){
-        window.alert('Passo inválido! Considerando PASSO 1')
-        fpas = 1
-    }*/
-    
-    //console.log('Vai começar')
-    //res.innerHTML = `<p>Contando: </p> ${fini.value}`
-
-    var c = 0
-    while(c <= 10){
-        res.innerHTML = 'Contando:'
-        c++
-    }
-
-    res.innerHTML = c+` oi`
-    /*
-    for (var c = 1; c <= 4; c++){
-        res.innerHTML = 'Contando:'
-        res.innerHTML = c
-        //console.log(c)
-    }
-    //console.log('Fim')
-        //document.querySelector(".element").innerHTML = "👉 &#x1F61C" 
-    */
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var passo   = Number(fpas.value)
+        var inicio  = Number(fini.value)
+        var final   = Number(ffim.value)
+        if(passo <= 0){
+            window.alert('Passo inválido! Considerando passo 1')
+            passo = 1
+        }
+        if(inicio < final){
+            for (var i = inicio; i <= final; i = i + passo){ // Contagem crescente
+                res.innerHTML += ` ${i} \u{1F449}`
+            }
+            
+        } else{
+            for(let i = inicio; i >= final; i = i - passo){ // Contagem regressiva
+                res.innerHTML += ` ${i} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    } 
 }
